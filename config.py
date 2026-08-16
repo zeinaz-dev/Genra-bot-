ab54b7355c7cb9471ba69ddcdcfcc2ef6a1fdd7f
zeinaz-dev <mariemtakrouni@gmail.com>
2026-08-16 03:28:19 +0100

    Simplify error message for missing DISCORD_TOKEN

--- config.py
+++ config.py
@@ -6,14 +6,4 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 if not TOKEN:
-    raise ValueError("DISCORD_TOKEN is missing from .env")
-
-STAFF_ROLE_IDS = [
-    int(x.strip())
-    for x in os.getenv("STAFF_ROLE_IDS", "").split(",")
-    if x.strip()
-]
-
-CLASH_ROLE_ID = int(os.getenv("CLASH_ROLE_ID", "0"))
-EMPIRE_ROLE_ID = int(os.getenv("EMPIRE_ROLE_ID", "0"))
-TRAINING_ROLE_ID = int(os.getenv("TRAINING_ROLE_ID", "0"))
+    raise ValueError("DISCORD_TOKEN is missing")
